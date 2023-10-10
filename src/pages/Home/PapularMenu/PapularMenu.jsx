f4125ddd6b608eb75dd6b24ac172ba8651dc752b
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-10-10 11:42:21 +0600

    custom hook

--- src/pages/Home/PapularMenu/PapularMenu.jsx
+++ src/pages/Home/PapularMenu/PapularMenu.jsx
@@ -1,23 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import SectionTitle from '../../../components/SectionTitle'
 import MenuItemCard from '../../Shared/MenuItemCard/MenuItemCard'
+import useMenu from '../../../hooks/useMenu'
 
 const PapularMenu = () => {
-  const [menu, setMenu] = useState([])
-  useEffect(() => {
-    fetch('menu.json')
-      .then(res => res.json())
-      .then(data => {
-        const popularItem = data.filter(pItem => pItem.category == 'popular')
-        setMenu(popularItem)
-      })
-  }, [])
+  const [menu] = useMenu()
+  const popularItem = menu.filter(pItem => pItem.category == 'popular')
+
   return (
     <div>
       <section>
         <SectionTitle subHeading={"---Check it out---"} heading={"FROM OUR MENU"}></SectionTitle>
         <div className='grid gap-4 grid-cols-2'>
-          {menu.map(item => <MenuItemCard item={item} key={item._id}></MenuItemCard>)}
+          {popularItem.map(item => <MenuItemCard item={item} key={item._id}></MenuItemCard>)}
         </div>
 
       </section>
