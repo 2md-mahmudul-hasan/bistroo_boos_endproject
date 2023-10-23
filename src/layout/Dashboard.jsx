089ad02888d1b49fc8f5865b7d94eba18547655f
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-10-23 07:00:14 +0600

    mycart

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -12,12 +12,12 @@ const Dashboard = () => {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <ul className="menu p-4 w-80 min-h-full bg-[#112233] text-base-content">
           {/* Sidebar content here */}
           <li><Link><FaUserShield></FaUserShield>User Home</Link></li>
           <li><Link><FaCalendar></FaCalendar>Reservation</Link></li>
           <li><Link><FaWallet></FaWallet>Wallet</Link></li>
-          <li><Link><FaShoppingCart></FaShoppingCart>mycart</Link></li>
+          <li><Link to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>mycart</Link></li>
           <div className="divider">......</div>
           <li><Link><FaHome></FaHome>Home</Link></li>
           <li><Link><FaList></FaList>Menu</Link></li>
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,9 +1,20 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
 
 
 const MyCart = () => {
+  const [cart] = useCart()
+  const total = cart.reduce((sum, item) => item.price + sum, 0)
   return (
     <div>
-      <p> My Cart </p>
+      <Helmet>
+        <title>bistroo:my-cart</title>
+      </Helmet>
+      <div className="text-center">
+        <h1> my total cart = {cart.length}</h1>
+        <h2> total price is {total}</h2>
+      </div>
+
     </div>
   );
 };
